c0383f29f8313c85a6d48871bda2c834900020d8
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-15 20:30:49 +0300

    Update 'AuthProviders' component

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react';
+import { getProviders } from 'next-auth/react';
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+
+            setProviders(res);
+        }
+
+        fetchProviders();
+    }, []);
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, i) => (
+                    <Button />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default AuthProviders;
