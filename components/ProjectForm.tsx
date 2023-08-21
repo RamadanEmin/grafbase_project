86161617996e3f413fcc3e30035accc01b3b5a9f
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-21 20:13:01 +0300

    Added 'handleChangeImage' function

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -57,7 +57,27 @@ const ProjectForm = ({ type, session, project }: Props) => {
     };
 
     const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
 
+        const file = e.target.files?.[0];
+
+        if (!file) {
+            return;
+        }
+
+        if (!file.type.includes('image')) {
+            return alert('Please upload an image file');
+        }
+
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+            const result = reader.result as string;
+
+            handleStateChange('image', result);
+        };
     };
 
     const handleStateChange = (fieldName: string, value: string) => {
