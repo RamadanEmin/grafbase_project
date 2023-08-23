edcf3318e1424aecc66e7044fb2959b5a15e8665
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-23 16:51:28 +0300

    Added 'fetchAllProjects' function

--- graphql/index.ts
+++ graphql/index.ts
@@ -30,6 +30,36 @@ export const createUserMutation = `
 	}
 `;
 
+export const projectsQuery = `
+    query getProjects($category: String, $endcursor: String) {
+        projectSearch(first: 4, after: $endcursor, filter: {category: {eq: $category}}) {
+            pageInfo {
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
+            }
+            edges {
+                node {
+                    title
+                    githubUrl
+                    description
+                    liveSiteUrl
+                    id
+                    image
+                    category
+                    createdBy {
+                        id
+                        email
+                        name
+                        avatarUrl
+                    }
+                }
+            }
+        }
+    }
+`;
+
 export const getUserQuery = `
     query GetUser($email: String!) {
         user(by: { email: $email }) {
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,4 +1,4 @@
-import { createProjectMutation, createUserMutation, getUserQuery } from '@/graphql';
+import { createProjectMutation, createUserMutation, getUserQuery, projectsQuery } from '@/graphql';
 import { GraphQLClient } from 'graphql-request';
 
 import { ProjectForm } from '@/common.types';
@@ -85,4 +85,10 @@ export const createNewProject = async (form: ProjectForm, creatorId: string, tok
 
         return makeGraphQLRequest(createProjectMutation, variables);
     }
+};
+
+export const fetchAllProjects = async (category?: string, endcursor?: string) => {
+    client.setHeader('x-api-key', apiKey);
+
+    return makeGraphQLRequest(projectsQuery, { category, endcursor });
 };
