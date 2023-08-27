21e600665e12d84478fd62ac508108aa5c2b3a41
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-27 08:57:51 +0300

    Added 'Categories' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ProjectInterface } from "@/common.types";
+import Categories from "@/components/Categories";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
@@ -22,7 +23,7 @@ const Home = async() =>{
     if (projectsToDisplay.length === 0) {
         return (
             <section className="flexStart flex-col paddings">
-                Categories
+                <Categories />
 
                 <p className="no-result-text text-center">No projects found, go create some first.</p>
             </section>
@@ -33,7 +34,7 @@ const Home = async() =>{
 
     return (
         <section className="flex-start flex-col paddings mb-16">
-            <h1>Categories</h1>
+             <Categories />
             
             <section className="projects-grid">
                 {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
